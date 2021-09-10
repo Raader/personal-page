39f5e893301e2a8c40180875df60d3725ba08d5b
Efe <48628713+Raader@users.noreply.github.com>
2021-09-10 19:14:52 +0300

    Create button component

---
+++ components/button.js
@@ -0,0 +1,10 @@
+export default function Button({ children, className, ...props }) {
+  return (
+    <button
+      className={`appearance-none p-1 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition duration-200 ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
