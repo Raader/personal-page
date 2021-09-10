c1dd9a601a2ec1cd44a365c0d78626136f221430
Efe <48628713+Raader@users.noreply.github.com>
2021-09-10 19:16:45 +0300

    Create layout component

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import Navbar from "./navbar";
+import { useState } from "react";
+
+export default function Layout({ children }) {
+  const [collapsed, setCollapsed] = useState(true);
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
+      <Navbar></Navbar>
+      <div className="container mx-auto">{children}</div>
+    </div>
+  );
+}
