20ca8048a2bd4e8cae0eeb6fb6770e35c1ef7762
Efe <48628713+Raader@users.noreply.github.com>
2021-09-10 19:15:17 +0300

    Create brand component

---
+++ components/brand.js
@@ -0,0 +1,12 @@
+export default function Brand() {
+  return (
+    <div className="text-3xl md:text-4xl font-archivo uppercase text-yellow-300">
+      <span>r</span>
+      <span>a</span>
+      <span>a</span>
+      <span>d</span>
+      <span>e</span>
+      <span>r</span>
+    </div>
+  );
+}
