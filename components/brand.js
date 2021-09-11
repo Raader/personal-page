8e051211a51354632bced5b5fd45c00e1127abdc
Efe <48628713+Raader@users.noreply.github.com>
2021-09-11 14:23:34 +0300

    Use accent color context on brand

--- components/brand.js
+++ components/brand.js
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import ThemeContext from "./theme_context";
+
 export default function Brand() {
+  const { accentColor } = useContext(ThemeContext);
   return (
-    <div className="text-3xl  xl:text-4xl font-archivo uppercase text-yellow-300">
+    <div
+      className={`text-3xl xl:text-4xl font-archivo uppercase`}
+      style={{ color: accentColor }}
+    >
       <span>r</span>
       <span>a</span>
       <span>a</span>
