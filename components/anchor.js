7cd9da25b1c625b84a9c9fa19f83e220aa47532d
Efe <48628713+Raader@users.noreply.github.com>
2021-09-11 14:54:51 +0300

    Add classname prop to anchor

--- components/anchor.js
+++ components/anchor.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 
-export default function Anchor({ children, ...props }) {
+export default function Anchor({ children, className, ...props }) {
   return (
     <Link {...props}>
-      <a className="font-semibold text-gray-700 dark:text-gray-200 hover:text-black dark:hover:text-white transition duration-100">
+      <a
+        className={`font-semibold text-gray-700 dark:text-gray-200 hover:text-black dark:hover:text-white transition duration-100 ${className}`}
+      >
         {children}
       </a>
     </Link>
