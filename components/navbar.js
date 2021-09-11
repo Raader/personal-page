7c05e3cbce1e8079effaa18c8f561f721c9b654d
Efe <48628713+Raader@users.noreply.github.com>
2021-09-11 13:42:57 +0300

    Update navbar to use links from an array

--- components/navbar.js
+++ components/navbar.js
@@ -1,33 +1,79 @@
-import { MenuIcon } from "@heroicons/react/solid";
+import {
+  DocumentTextIcon,
+  FolderAddIcon,
+  FolderIcon,
+  HomeIcon,
+  IdentificationIcon,
+  MailIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
 import { SunIcon } from "@heroicons/react/outline";
 import Anchor from "./anchor";
 import Brand from "./brand";
 import { useState } from "react";
 import Button from "./button";
+import IconText from "./icon_text";
+import NavLink from "./nav_link";
 
 export default function Navbar({ children }) {
   const [collapsed, setCollapsed] = useState(true);
+  const links = [
+    {
+      name: "Home",
+      href: "/",
+      icon: <HomeIcon className="w-5 h-5 xl:w-6 xl:h-6"></HomeIcon>,
+    },
+    {
+      name: "Projects",
+      href: "/",
+      icon: <FolderIcon className="w-5 h-5 xl:w-6 xl:h-6"></FolderIcon>,
+    },
+    {
+      name: "Articles",
+      href: "/",
+      icon: (
+        <DocumentTextIcon className="w-5 h-5 xl:w-6 xl:h-6"></DocumentTextIcon>
+      ),
+    },
+    {
+      name: "About",
+      href: "/",
+      icon: (
+        <IdentificationIcon className="w-5 h-5 xl:w-6 xl:h-6"></IdentificationIcon>
+      ),
+    },
+    {
+      name: "Contact",
+      href: "/",
+      icon: <MailIcon className="w-5 h-5 xl:w-6 xl:h-6"></MailIcon>,
+    },
+  ];
 
   return (
-    <nav className="w-full p-4 border-b-4 border-yellow-300">
+    <nav className="w-full p-4 md:p-6 border-b-4 border-yellow-300">
       <div className="container mx-auto flex items-center">
         <Brand></Brand>
-        <div className="mx-16 hidden md:flex md:space-x-8 text-xl">
-          <Anchor href="/">Home</Anchor>
-          <Anchor href="/">Projects</Anchor>
-          <Anchor href="/">Articles</Anchor>
-          <Anchor href="/">About</Anchor>
+        <div className="mx-16 hidden md:flex md:space-x-8 text-lg xl:text-xl">
+          {links.map((link, index) => (
+            <NavLink
+              href={link.href}
+              icon={link.icon}
+              key={`nav-link-${index}`}
+            >
+              {link.name}
+            </NavLink>
+          ))}
         </div>
 
         <div className="ml-auto flex space-x-4">
           <Button
-            className=""
+            className="block"
             onClick={() => document.documentElement.classList.toggle("dark")}
           >
             <SunIcon className="w-8 h-8"></SunIcon>
           </Button>
           <Button
-            className="lg:hidden"
+            className="md:hidden"
             onClick={() => setCollapsed((prev) => !prev)}
           >
             <MenuIcon className="w-8 h-8"></MenuIcon>
@@ -36,10 +82,15 @@ export default function Navbar({ children }) {
       </div>
       {!collapsed ? (
         <div className="w-full flex flex-col space-y-2 text-xl mt-4">
-          <Anchor href="/">Home</Anchor>
-          <Anchor href="/">Projects</Anchor>
-          <Anchor href="/">Articles</Anchor>
-          <Anchor href="/">About</Anchor>
+          {links.map((link, index) => (
+            <NavLink
+              href={link.href}
+              icon={link.icon}
+              key={`drop-nav-link-${index}`}
+            >
+              {link.name}
+            </NavLink>
+          ))}
         </div>
       ) : (
         <div></div>
