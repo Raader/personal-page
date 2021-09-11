929e0cdc8282f998c037a19084eb1a6793b3afcb
Efe <48628713+Raader@users.noreply.github.com>
2021-09-11 13:42:27 +0300

    Update brand responsive font size

--- components/brand.js
+++ components/brand.js
@@ -1,6 +1,6 @@
 export default function Brand() {
   return (
-    <div className="text-3xl md:text-4xl font-archivo uppercase text-yellow-300">
+    <div className="text-3xl  xl:text-4xl font-archivo uppercase text-yellow-300">
       <span>r</span>
       <span>a</span>
       <span>a</span>
