97ceeeb90f01cc10b2d5c3c807973d49f67d6664
Efe <48628713+Raader@users.noreply.github.com>
2021-09-11 14:23:58 +0300

    Add a selected state to nav link and use accent color

--- components/nav_link.js
+++ components/nav_link.js
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import Anchor from "./anchor";
 import IconText from "./icon_text";
+import ThemeContext from "./theme_context";
 
-export default function NavLink({ href, icon, children }) {
+export default function NavLink({ href, icon, children, selected }) {
+  const { accentColor } = useContext(ThemeContext);
   return (
     <Anchor href={href}>
-      <IconText icon={icon}>{children}</IconText>
+      <IconText icon={icon} style={selected ? { color: accentColor } : {}}>
+        {children}
+      </IconText>
     </Anchor>
   );
 }
