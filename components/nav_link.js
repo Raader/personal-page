aac56638a5571d729fedf69cc8df2dc1c21742b5
Efe <48628713+Raader@users.noreply.github.com>
2021-09-11 14:55:13 +0300

    Add passing down props on nav link

--- components/nav_link.js
+++ components/nav_link.js
@@ -3,10 +3,10 @@ import Anchor from "./anchor";
 import IconText from "./icon_text";
 import ThemeContext from "./theme_context";
 
-export default function NavLink({ href, icon, children, selected }) {
+export default function NavLink({ href, icon, children, selected, ...props }) {
   const { accentColor } = useContext(ThemeContext);
   return (
-    <Anchor href={href}>
+    <Anchor href={href} {...props}>
       <IconText icon={icon} style={selected ? { color: accentColor } : {}}>
         {children}
       </IconText>
