d7568cf76e66f56b7006e091cc0ab04f7e65bb08
Efe <48628713+Raader@users.noreply.github.com>
2021-09-12 14:34:03 +0300

    Update anchor to pass props

--- components/anchor.js
+++ components/anchor.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-export default function Anchor({ children, className, ...props }) {
+export default function Anchor({ children, href, className, ...props }) {
   return (
-    <Link {...props}>
+    <Link href={href}>
       <a
         className={`font-semibold text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-200 ${className}`}
+        {...props}
       >
         {children}
       </a>
