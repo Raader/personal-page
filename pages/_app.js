0c11cb3d5e540088082d7b762ab535313323d24a
Efe <48628713+Raader@users.noreply.github.com>
2021-09-12 16:48:30 +0300

    Set theme on page load

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "dark")
+      document.documentElement.classList.add("dark");
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
