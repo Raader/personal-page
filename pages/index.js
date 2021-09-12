29eda96b5fbb8898779b96e0daab2da92849d027
Efe <48628713+Raader@users.noreply.github.com>
2021-09-12 15:06:46 +0300

    Create home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+import ThemeContext from "../components/theme_context";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>Raader</title>
+      </Head>
+      <ThemeContext.Provider value={{ accentColor: "#60A5FA" }}>
+        <Layout></Layout>
+      </ThemeContext.Provider>
+    </>
+  );
 }
