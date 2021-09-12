8379d0ec27a86ac3eabcb1fb575bb9857be04c6c
Efe <48628713+Raader@users.noreply.github.com>
2021-09-12 15:08:01 +0300

    Create projects page

---
+++ pages/projects/index.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/layout";
+import ThemeContext from "../../components/theme_context";
+import Head from "next/head";
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <Head>
+        <title>Projects | Raader</title>
+      </Head>
+      <ThemeContext.Provider value={{ accentColor: "#34D399" }}>
+        <Layout></Layout>
+      </ThemeContext.Provider>
+    </>
+  );
+}
