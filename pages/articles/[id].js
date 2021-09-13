3e9a7a4eb6305d0b598fe8fd3971bdbd9127d437
Efe <48628713+Raader@users.noreply.github.com>
2021-09-13 17:34:02 +0300

    Add description tag to article page

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -10,6 +10,7 @@ export default function Article({ post }) {
     <>
       <Head>
         <title>{post.title}</title>
+        <meta name="description" content={post.preview.replaceAll(/<p>|<\/p>/ig,"")}></meta>
       </Head>
       <ThemeContext.Provider value={{ accentColor: "#FBBF24" }}>
         <Layout>
