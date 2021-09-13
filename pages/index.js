2ef0d28550645e7d6881af933972d58d4301026d
Efe <48628713+Raader@users.noreply.github.com>
2021-09-13 17:32:38 +0300

    Add description meta tag to title

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
     <>
       <Head>
         <title>Efe Ağca | Personal Page</title>
+        <meta name="description" content="I'm a self taught full stack developer who's been coding /
+              developing opens source apps and libraries myself for over 4
+              years."></meta>
       </Head>
       <ThemeContext.Provider value={{ accentColor: "#60A5FA" }}>
         <Layout></Layout>
