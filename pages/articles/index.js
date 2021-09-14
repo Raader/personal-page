dec3c3885d726af651a4dddc307499124ad6f987
Efe <48628713+Raader@users.noreply.github.com>
2021-09-14 17:29:38 +0300

    Use article link component in articles page

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,15 +1,11 @@
-import { useContext } from "react";
-import Anchor from "../../components/anchor";
-import Button from "../../components/button";
-import FormattedDate from "../../components/formatted_date";
 import Layout from "../../components/layout";
 import Prose from "../../components/prose";
 import ThemeContext from "../../components/theme_context";
 import { getAllPosts } from "../../lib/posts";
 import Head from "next/head";
+import ArticleLink from "../../components/article_link";
 
 export default function Articles({ posts }) {
-  const { accentColor } = useContext(ThemeContext);
   return (
     <>
       <Head>
@@ -21,26 +17,7 @@ export default function Articles({ posts }) {
             <h1>Articles</h1>
             <div className="-mt-8">
               {posts.map((post, index) => (
-                <div key={`post-${index}`}>
-                  <div className="flex items-baseline flex-nowrap justify-between">
-                    <Anchor href={`/articles/${post.id}`}>
-                      <h3>{post.title}</h3>
-                    </Anchor>
-                    <FormattedDate>{post.date}</FormattedDate>
-                  </div>
-
-                  <div
-                    dangerouslySetInnerHTML={{ __html: post.preview }}
-                    className="-my-4 "
-                  ></div>
-
-                  <Anchor
-                    href={`/articles/${post.id}`}
-                    style={{ color: accentColor }}
-                  >
-                    Read More
-                  </Anchor>
-                </div>
+                <ArticleLink key={`post-${index}`} article={post}></ArticleLink>
               ))}
             </div>
           </Prose>
