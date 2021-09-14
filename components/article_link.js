a20f682509f2d6d0833a718a70124308562dbf92
Efe <48628713+Raader@users.noreply.github.com>
2021-09-14 17:29:20 +0300

    Create article link component

---
+++ components/article_link.js
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import Anchor from "./anchor";
+import FormattedDate from "./formatted_date";
+import ThemeContext from "./theme_context";
+
+export default function ArticleLink({ article, ...props }) {
+  const { accentColor } = useContext(ThemeContext);
+
+  return (
+    <div {...props}>
+      <div className="flex items-baseline flex-nowrap justify-between">
+        <Anchor href={`/articles/${article.id}`}>
+          <h3>{article.title}</h3>
+        </Anchor>
+        <FormattedDate>{article.date}</FormattedDate>
+      </div>
+
+      <div
+        dangerouslySetInnerHTML={{ __html: article.preview }}
+        className="-my-4 "
+      ></div>
+
+      <Anchor href={`/articles/${article.id}`} style={{ color: accentColor }}>
+        Read More
+      </Anchor>
+    </div>
+  );
+}
