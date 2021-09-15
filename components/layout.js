653e575875706ecc829d560d8c192f508a87a7b7
Efe <48628713+Raader@users.noreply.github.com>
2021-09-15 16:50:02 +0300

    Remove padding from layout

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
       <Navbar></Navbar>
-      <main className="container mx-auto p-4">{children}</main>
+      <main className="mx-auto">{children}</main>
     </div>
   );
 }
