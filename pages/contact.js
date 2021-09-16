237ee81962c7e0739ef2909d79ca74dc5a564500
Efe <48628713+Raader@users.noreply.github.com>
2021-09-16 17:41:57 +0300

    Add contact form to contact page
    Add max width to contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,9 @@
 import ThemeContext from "../components/theme_context";
 import Layout from "../components/layout";
 import Head from "next/head";
+import Section from "../components/section";
+import Prose from "../components/prose";
+import OutlinedButton from "../components/outlined_button";
 
 export default function ContactPage() {
   return (
@@ -9,7 +12,46 @@ export default function ContactPage() {
         <title>Contact | Efe Ağca</title>
       </Head>
       <ThemeContext.Provider value={{ accentColor: "#A78BFA" }}>
-        <Layout></Layout>
+        <Layout>
+          <Section>
+            <Prose>
+              <h1>Contact Me</h1>
+              <form
+                className="space-y-4 max-w-lg"
+                action="https://formspree.io/f/xayabgnl"
+                method="POST"
+              >
+                <label className="block">
+                  Full Name
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="Full Name"
+                    className="block w-full rounded-lg border-transparent focus:ring-[#A78BFA] focus:border-[#A78BFA] dark:bg-black"
+                  ></input>
+                </label>
+                <label className="block">
+                  Email
+                  <input
+                    name="_replyto"
+                    type="email"
+                    placeholder="Your Email"
+                    className="block w-full rounded-lg border-transparent focus:ring-[#A78BFA] focus:border-[#A78BFA] dark:bg-black"
+                  ></input>
+                </label>
+                <label className="block">
+                  Message
+                  <textarea
+                    name="message"
+                    className="block w-full rounded-lg border-transparent focus:ring-[#A78BFA] focus:border-[#A78BFA] dark:bg-black"
+                    rows="5"
+                  ></textarea>
+                </label>
+                <OutlinedButton type="submit">Send Message</OutlinedButton>
+              </form>
+            </Prose>
+          </Section>
+        </Layout>
       </ThemeContext.Provider>
     </>
   );
