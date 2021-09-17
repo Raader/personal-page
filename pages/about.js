dac1063192779ebccacb75ccc5a33255bc8a7d46
Efe <48628713+Raader@users.noreply.github.com>
2021-09-17 23:15:22 +0300

    Create basic about page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import ThemeContext from "../components/theme_context";
 import Layout from "../components/layout";
 import Head from "next/head";
+import Section from "../components/section";
+import Prose from "../components/prose";
 
 export default function AboutPage() {
   return (
@@ -9,7 +11,18 @@ export default function AboutPage() {
         <title>About | Efe Ağca</title>
       </Head>
       <ThemeContext.Provider value={{ accentColor: "#FBBF24" }}>
-        <Layout></Layout>
+        <Layout>
+          <Section>
+            <Prose>
+              <h1>About Me</h1>
+              <p>
+                {`I'm Efe Ağca, a self taught full stack developer who's been
+                coding / developing opens source apps and libraries myself for
+                over 4 years.`}
+              </p>
+            </Prose>
+          </Section>
+        </Layout>
       </ThemeContext.Provider>
     </>
   );
