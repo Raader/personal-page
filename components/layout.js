01eb5effad2e111bdce0c210204cb7c29201ba7f
Efe <48628713+Raader@users.noreply.github.com>
2021-09-17 23:14:49 +0300

    Add minimum height to main body

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
       <Navbar></Navbar>
-      <main className="mx-auto">{children}</main>
+      <main className="mx-auto min-h-screen">{children}</main>
     </div>
   );
 }
