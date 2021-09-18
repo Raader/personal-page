ba34938dedfa320333a9dd36d257aed67517fd41
Efe <48628713+Raader@users.noreply.github.com>
2021-09-18 14:34:20 +0300

    Format footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,11 @@ import Section from "./section";
 
 export default function Footer({ className, ...props }) {
   return (
-    <Section className={`my-0 py-0 xl:my-0 ${className}`} {...props}>
+    <Section
+      className={`my-0 py-0 xl:my-0
+     ${className}`}
+      {...props}
+    >
       <Prose>
         <div className="flex">
           <p>© 2021 Efe Ağca</p>
