fe881320e6d71d17ded0b8bb0493b9fa2262d162
Efe <48628713+Raader@users.noreply.github.com>
2021-09-18 16:13:16 +0300

    Add github link to footer

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,5 @@
+import Anchor from "./anchor";
+import GithubIcon from "./icons/github_icon";
 import Prose from "./prose";
 import Section from "./section";
 
@@ -9,9 +11,13 @@ export default function Footer({ className, ...props }) {
       {...props}
     >
       <Prose>
-        <div className="flex">
+        <div className="flex justify-between items-center">
           <p>© 2021 Efe Ağca</p>
-          <div className="flex"></div>
+          <div className="flex">
+            <a href="https://github.com/Raader">
+              <GithubIcon className="w-5 h-5"></GithubIcon>
+            </a>
+          </div>
         </div>
       </Prose>
     </Section>
