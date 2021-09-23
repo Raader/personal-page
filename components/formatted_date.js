51e70ebd835af10de19f923238fe5254d91cd98f
Efe <48628713+Raader@users.noreply.github.com>
2021-09-23 23:47:07 +0300

    Update formatted dates format style

--- components/formatted_date.js
+++ components/formatted_date.js
@@ -1,12 +1,20 @@
 import { useContext } from "react";
 import ThemeContext from "./theme_context";
 
-export default function FormattedDate({ children, ...props }) {
+export default function FormattedDate({ children, className, ...props }) {
   const { accentColor } = useContext(ThemeContext);
 
   return (
-    <p style={{ color: accentColor }} {...props}>
-      {new Date(children).toLocaleDateString()}
+    <p
+      style={{ color: accentColor }}
+      className={`whitespace-nowrap ${className}`}
+      {...props}
+    >
+      {new Date(children).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}
     </p>
   );
 }
