f63575245174311295c9c00b70e1d4b31e77b620
Efe <48628713+Raader@users.noreply.github.com>
2021-09-30 21:52:37 +0300

    Update collapse to make it more customizable

--- components/collapse.js
+++ components/collapse.js
@@ -4,16 +4,20 @@ export default function Collapse({
   collapsed,
   animationDuration = "0.3s",
   children,
+  style,
+  className,
   ...props
 }) {
   const ref = useRef();
   return (
     <div
       ref={ref}
+      className={`${collapsed ? "max-h-0 md:max-h-full" : ""} ${className}`}
       style={{
         overflow: "hidden",
-        maxHeight: !collapsed ? `${ref.current?.scrollHeight}px` : "0",
+        maxHeight: !collapsed ? `${ref.current?.scrollHeight}px` : undefined,
         transition: `max-height ${animationDuration} ease-out`,
+        ...style,
       }}
       {...props}
     >
