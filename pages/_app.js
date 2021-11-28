10c7705c1ab070ac7854fc76a624f67f063f351c
Efe <48628713+Raader@users.noreply.github.com>
2021-11-28 15:21:43 +0300

    Add google analytics scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { useEffect } from "react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -8,7 +9,25 @@ function MyApp({ Component, pageProps }) {
       document.documentElement.classList.add("dark");
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7V3TZ07N9L"
+      />
+      <Script
+        id="analytics"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-7V3TZ07N9L');`,
+        }}
+      ></Script>
+    </>
+  );
 }
 
 export default MyApp;
